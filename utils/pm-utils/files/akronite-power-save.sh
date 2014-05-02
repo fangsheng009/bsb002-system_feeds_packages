3cd0cbc5f06f91fcacaaa8558c765f569bbe26b0
Mohammmed Shafi Shajakhan <mohammed@codeaurora.org>
2014-05-02 17:44:22 +0530

    [pm-utils] Revamp akronite-power-save.sh
    *iflan up takes care of bringing NSS connection manager
    and wifi, so remove redundant code.
    *changes in value for Clock Power up sequence.
    *Fix the order where Clock Power down sequence should be called.
    *Order of removing the devices : PCIe, SATA, USB.
    
    Change-Id: Iaec9a83d93e68e03183edb8bd8beec32f5046dc4
    Signed-off-by: Vijay Pamidipati <vpamidip@codeaurora.org>
    Signed-off-by: Mohammmed Shafi Shajakhan <mohammed@codeaurora.org>

--- utils/pm-utils/files/akronite-power-save.sh
+++ utils/pm-utils/files/akronite-power-save.sh
@@ -11,31 +11,23 @@ ac_power()
 	echo 64000000 > /sys/kernel/debug/clk/dfab_a_clk/rate
 	echo 64000000 > /sys/kernel/debug/clk/sfpb_a_clk/rate
 	echo 64000000 > /sys/kernel/debug/clk/cfpb_a_clk/rate
-	echo 400000000 > /sys/kernel/debug/clk/nssfab0_a_clk/rate
-	echo 200000000 > /sys/kernel/debug/clk/nssfab1_a_clk/rate
+	echo 133000000 > /sys/kernel/debug/clk/nssfab0_a_clk/rate
+	echo 133000000 > /sys/kernel/debug/clk/nssfab1_a_clk/rate
 	echo 400000000 > /sys/kernel/debug/clk/ebi1_a_clk/rate
 
 # Enabling Auto scale on NSS cores
 	echo 1 > /proc/sys/dev/nss/clock/auto_scale
 
 # PCIe Power-UP Sequence
+	sleep 1
 	echo 1 > /sys/bus/pci/rcrescan
 	sleep 2
 	echo 1 > /sys/bus/pci/rescan
 
-# NSS Power-UP Sequence
-
-# Load connection manager module
-	[ -d /sys/module/qca_nss_connmgr_ipv4 ] || insmod qca-nss-connmgr-ipv4
 	sleep 1
-	[ -d /sys/module/qca_nss_connmgr_ipv6 ] || insmod qca-nss-connmgr-ipv6
 
 # Bringing Up LAN Interface
 	ifup lan
-
-# Wifi power up sequence
-	wifi up
-
 
 # Sata Power-UP Sequence
 	[ -f /sys/devices/platform/msm_sata.0/ahci.0/msm_sata_suspend ] && {
@@ -57,48 +49,33 @@ battery_power()
 # Wifi Power-down Sequence
 	wifi down
 
-# NSS Power-down Sequence
-
-# Bringing Down LAN Interface
+# Bring Down LAN Interface
 	ifdown lan
 
-# Stop and Unload connection manager module
+# PCIe Power-Down Sequence
 
-	[ -d /sys/module/qca_nss_connmgr_ipv6 ] && {
-		echo 1 > /sys/nom_v6/stop
-		sleep 1
-		echo f > /proc/net/nf_conntrack
-		sleep 3
-		echo 1 > /sys/nom_v6/terminate
-		sleep 2
-		rmmod qca_nss_connmgr_ipv6
+# Remove devices
+	sleep 2
+	for i in `ls /sys/bus/pci/devices/`; do
+		d=/sys/bus/pci/devices/${i}
+		v=`cat ${d}/vendor`
+		[ "xx${v}" != "xx0x17cb" ] && echo 1 > ${d}/remove
+	done
+
+# Remove Buses
+	sleep 2
+	for i in `ls /sys/bus/pci/devices/`; do
+		d=/sys/bus/pci/devices/${i}
+		echo 1 > ${d}/remove
+	done
+
+# Remove RC
+	sleep 2
+
+	[ -f /sys/devices/pci0000:00/pci_bus/0000:00/rcremove ] && {
+		echo 1 > /sys/devices/pci0000:00/pci_bus/0000:00/rcremove
 	}
-
-	[ -d /sys/module/qca_nss_connmgr_ipv4 ] && {
-		echo 1 > /sys/nom_v4/stop
-		sleep 1
-		echo f > /proc/net/nf_conntrack
-		sleep 3
-		echo 1 > /sys/nom_v4/terminate
-		sleep 2
-		rmmod qca_nss_connmgr_ipv4
-	}
-
-# Scaling Down UBI Cores
-	echo 110000000 > /proc/sys/dev/nss/clock/current_freq
-
-# Disabling Auto scale on NSS cores
-	echo 0 > /proc/sys/dev/nss/clock/auto_scale
-
-# Clocks Power-down Sequence
-
-	echo 400000000 > /sys/kernel/debug/clk/afab_a_clk/rate
-	echo 32000000 > /sys/kernel/debug/clk/dfab_a_clk/rate
-	echo 32000000 > /sys/kernel/debug/clk/sfpb_a_clk/rate
-	echo 32000000 > /sys/kernel/debug/clk/cfpb_a_clk/rate
-	echo 133000000 > /sys/kernel/debug/clk/nssfab0_a_clk/rate
-	echo 133000000 > /sys/kernel/debug/clk/nssfab1_a_clk/rate
-	echo 400000000 > /sys/kernel/debug/clk/ebi1_a_clk/rate
+	sleep 1
 
 # Find scsi devices and remove it
 
@@ -121,32 +98,25 @@ battery_power()
 
 # USB Power-down Sequence
 	[ -d /sys/module/dwc3_ipq ] && rmmod dwc3-ipq
+	sleep 1
 
-# PCIe Power-Down Sequence
+# Disabling Auto scale on NSS cores
+	echo 0 > /proc/sys/dev/nss/clock/auto_scale
 
-# Remove devices
-	for i in `ls /sys/bus/pci/devices/`; do
-		d=/sys/bus/pci/devices/${i}
-		v=`cat ${d}/vendor`
-		[ "xx${v}" != "xx0x17cb" ] && echo 1 > ${d}/remove
-	done
+# Clocks Power-down Sequence
 
-	sleep 2
+	echo 400000000 > /sys/kernel/debug/clk/afab_a_clk/rate
+	echo 32000000 > /sys/kernel/debug/clk/dfab_a_clk/rate
+	echo 32000000 > /sys/kernel/debug/clk/sfpb_a_clk/rate
+	echo 32000000 > /sys/kernel/debug/clk/cfpb_a_clk/rate
+	echo 133000000 > /sys/kernel/debug/clk/nssfab0_a_clk/rate
+	echo 133000000 > /sys/kernel/debug/clk/nssfab1_a_clk/rate
+	echo 400000000 > /sys/kernel/debug/clk/ebi1_a_clk/rate
 
-# Remove Buses
-	for i in `ls /sys/bus/pci/devices/`; do
-		d=/sys/bus/pci/devices/${i}
-		echo 1 > ${d}/remove
-	done
-
-# Remove RC
-
-	[ -f /sys/devices/pci0000:00/pci_bus/0000:00/rcremove ] && {
-		echo 1 > /sys/devices/pci0000:00/pci_bus/0000:00/rcremove
-	}
+# Scaling Down UBI Cores
+	echo 110000000 > /proc/sys/dev/nss/clock/current_freq
 
 # Krait Power-down Sequence
-
 	echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 	echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 	echo "powersave" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
